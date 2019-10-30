2b1e87f65336b1f3592599efe9197622b9ab89ce
Akim Clerge <akimclerge@gmail.com>
2019-10-30 17:58:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,11 @@
 # }
 
 def happy_birthday(birthday_kids)
+  if age > 12
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
+  
 end
 
 
